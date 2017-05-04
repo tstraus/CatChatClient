f6bf1cfcc200aa7c07db28f428b0f22d123dc4a2
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2017-05-03 21:45:52 -0400

    tstraus.net

--- web/ws_client.js
+++ web/ws_client.js
@@ -3,26 +3,26 @@ var ws;
 window.onload = function() {
     document.getElementById("input").addEventListener("keyup", function(event) {
         event.preventDefault();
-    
+
         if (event.keyCode == 13) {
             document.getElementById("sendButton").click();
         }
     });
 
-    ws = new WebSocket("ws://127.0.0.1:1234");
+    ws = new WebSocket("ws://tstraus.net:1234");
 
     ws.onopen = function (event) {
         var join = {
             user: "Tuckles",
             type: "join"
         };
-    
+
         ws.send(JSON.stringify(join));
     }
 
     ws.onmessage = function (event) {
         var msg = JSON.parse(event.data);
-        
+
         if (msg.type === "msg") {
             console.log(msg.user + ": \"" + msg.data + "\"");
         }
