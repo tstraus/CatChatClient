4e660928b96c8b0455e1afbd441f3a48f6281d28
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2017-05-07 19:18:53 -0400

    user connect button and destination field

--- web/ws_client.js
+++ web/ws_client.js
@@ -9,15 +9,16 @@ window.onload = function() {
         }
     });
 
-    ws = new WebSocket("ws://tstraus.net:1234");
+    //ws = new WebSocket("ws://tstraus.net:1234");
+    ws = new WebSocket("ws://127.0.0.1:1234");
 
     ws.onopen = function (event) {
-        var join = {
+        /*var join = {
             user: "Tuckles",
             type: "join"
         };
 
-        ws.send(JSON.stringify(join));
+        ws.send(JSON.stringify(join));*/
     }
 
     ws.onmessage = function (event) {
@@ -27,13 +28,25 @@ window.onload = function() {
             console.log(msg.user + ": \"" + msg.data + "\"");
         }
     };
-}
+};
+
+function sendConnect() {
+    var join = {
+        user: document.getElementById("user").value,
+        type: "join"
+    };
+
+    ws.send(JSON.stringify(join));
+};
 
 function sendMessage() {
     var msg = {
-        user: "Tuckles",
+        user: document.getElementById("user").value,
         type: "msg",
-        data: document.getElementById("input").value
+        data: {
+            dest: document.getElementById("dest").value,
+            msg: document.getElementById("input").value
+        }
     };
 
     ws.send(JSON.stringify(msg));
