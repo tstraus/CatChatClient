00147b91ba4c24ba08f6a9442fdecf6ee105c73b
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2017-05-30 20:04:14 -0400

    changed var to const and let

--- web/ws_client.js
+++ web/ws_client.js
@@ -1,47 +1,47 @@
-var ws;
+let ws;
 
 window.onload = function() {
-    document.getElementById("input").addEventListener("keyup", function(event) {
+    document.getElementById('input').addEventListener('keyup', function(event) {
         event.preventDefault();
 
         if (event.keyCode == 13) {
-            document.getElementById("sendButton").click();
+            document.getElementById('sendButton').click();
         }
     });
 
-    ws = new WebSocket("ws://tstraus.net:1234");
-    //ws = new WebSocket("ws://127.0.0.1:1234");
+    //ws = new WebSocket('ws://tstraus.net:1234');
+    ws = new WebSocket('ws://127.0.0.1:1234');
 
     ws.onopen = function (event) {
-        console.log("Connected");
+        console.log('Connected');
     }
 
     ws.onmessage = function (event) {
-        var msg = JSON.parse(event.data);
+        const msg = JSON.parse(event.data);
 
-        if (msg.type === "msg") {
-            console.log(msg.user + ": \"" + msg.data + "\"");
+        if (msg.type === 'msg') {
+            console.log(msg.user + ': \'' + msg.data + '\'');
         }
     };
 };
 
 function sendJoin() {
-    var join = {
-        type: "join",
-        user: document.getElementById("user").value
+    const join = {
+        type: 'join',
+        user: document.getElementById('user').value
     };
 
     ws.send(JSON.stringify(join));
 };
 
 function sendMessage() {
-    var msg = {
-        type: "msg",
-        user: document.getElementById("dest").value,
-        data: document.getElementById("input").value
+    const msg = {
+        type: 'msg',
+        user: document.getElementById('dest').value,
+        data: document.getElementById('input').value
     };
 
     ws.send(JSON.stringify(msg));
 
-    document.getElementById("input").value = "";
+    document.getElementById('input').value = '';
 };
